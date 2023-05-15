a89d11fbdebc351f0d00d84511db0429b0a13fd0
Greg Szumel <greg.szumel@gmail.com>
2023-05-15 11:58:59 -0400

    cifar .5 accuracy

--- run.jl
+++ run.jl
@@ -8,8 +8,8 @@ import MLUtils
 include("models.jl")  # S4Model
 
 # setting up parameters
-learning_rate = 0.01
-weight_decay = 0.01
+learning_rate = 0.1
+weight_decay = 0.1
 
 epochs = 100
 dataset = :cifar10
@@ -26,56 +26,81 @@ device = "cpu";
 best_acc = 0; start_epoch = 0;
 
 model = S4Model(1024, 10, d_model)  # d_model = 1024, d_state = 64
+# model = Chain(
+#     S4D(1024),  # L, H, B -> L, H, B
+#     Flux.flatten,
+#     Flux.Dense(3*1024, 10)
+# )
 
 train = CIFAR10(split=:train)[:]
 test = CIFAR10(split=:test)[:]
 train, val = MLUtils.splitobs((train.features, train.targets), at=0.9)
 
-train_dataloader = Flux.DataLoader(train, batchsize=batch_size)
-val_dataloader = Flux.DataLoader(val, batchsize=batch_size)
-test_dataloader = Flux.DataLoader(test, batchsize=batch_size)
+train_dataloader = Flux.DataLoader(train, batchsize=batch_size, partial=false)
+val_dataloader = Flux.DataLoader(val, batchsize=batch_size, partial=false)
+test_dataloader = Flux.DataLoader(test, batchsize=batch_size, partial=false)
+
 
 loss(x, y) = sum(crossentropy(x, y))
-accuracy(x, y) = mean(onecold(m(x), 0:9) .== onecold(y, 0:9))
+accuracy(x, y) = mean(onecold(model(x), 0:9) .== y)
 opt_state = Flux.setup(Adam(), model)
 
 epochs = 10
 
-for (input, label) in train_dataloader
-    print(input |> size, label |>size)
-    global output = model(reshape(input, (3, 1024, batch_size)))
-    global i = input
-    global l = label
-    break
+function test_forward()
+    for (input, label) in train_dataloader
+        print(input |> size, label |>size)
+        global output = model(reshape(input, (3, 1024, batch_size)))
+        global i = input
+        global l = label
+        break
+    end
+    return true
 end
+
+function test_forward_backward()
+    for (input, label) in train_dataloader
+        gs = Flux.gradient(model) do m
+            output = m(reshape(input, (3, 1024, batch_size)))
+            l = loss(Flux.softmax(output), onehotbatch(label, 0:9))
+            println(l)
+            global outputs = output
+            l
+        end
+        Flux.update!(opt_state, model, gs[1])
+        return input, label, outputs
+    end
+end
+
+function test_convergence()
+    for _ in 1:100
+        test_forward_backward()
+    end
+end
+
 output
-loss(Flux.sigmoid.(output), onehotbatch(l, 0:9))
+loss(Flux.softmax(output), onehotbatch(label, 0:9))
+sum(onecold(Flux.softmax(output)) .- 1 .== label)
+output[:, 1]
+label[1]
 
-for (input, label) in train_dataloader
-    print(input |> size, label |>size)
-    gs = Flux.gradient(model) do m
-        output = m(reshape(input, (3, 1024, batch_size)))
-        l = loss(Flux.sigmoid.(output), onehotbatch(label, 0:9))
-        println(l)
-        l
+
+for _ in 1:epochs
+    for (input, label) in train_dataloader
+        gs = Flux.gradient(model) do m
+            output = Flux.softmax(m(reshape(input, (3, 1024, batch_size))))
+            l = loss(output, onehotbatch(label, 0:9))
+            l
+        end
+        Flux.update!(opt_state, model, gs[1])
     end
-    Flux.update!(opt_state, model, gs[1])
-    break
+    correct = 0
+    total = 0
+    for (input, label) in val_dataloader
+        output = model(reshape(input, (3, 1024, batch_size)))
+        correct += sum(onecold(Flux.softmax(output)) .- 1 .== label)
+        total += 32
+    end
+    println(correct / total)
 end
-@show accuracy(valX, valY)
-
-
-for epoch = 1:epochs
-    for d in zip(train_x, labels)
-      inputs, labels = d
-      gs = Flux.gradient(model) do m
-        l = loss(m(inputs), labels)
-        println(l)
-        l
-      end
-      Flux.update!(opt_state, model, gs[1])
-    end
-    @show accuracy(valX, valY)
-  end
-  
-
+ # 0.5 accuracy
